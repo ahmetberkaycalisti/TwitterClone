cc50cf649cd5e81c35f7d7fa6faadba5b0688a5a
ahmetberkaycalisti <ahmetberkaycalisti@gmail.com>
2018-06-14 13:51:08 +0800

    Clone with swift

---
+++ TwitterClone/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  TwitterClone
+//
+//  Created by Ahmet Berkay CALISTI on 14/06/2018.
+//  Copyright © 2018 Ahmet Berkay CALISTI. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
